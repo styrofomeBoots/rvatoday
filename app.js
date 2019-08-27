931b553b32451db84d2b952dcdefb3cbd9f3076e
Seth <seth.newbill@temperpack.com>
2019-08-27 12:30:43 -0400

    wedding locations

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
+// vmfa
+// toast
+// greenleafs
+// sugar and twine
+// belle isle
+// hollywood cemetery 
+// pipeline
+// bandy field
+// pony pasture
+
+const blurbInfo = {
+    37.5599 : ["Bandito's Bar and Lounge", 
+                "Seth's favorite place to get tacos and margaritas.  Awesome punk bar in a small bar district called Devil's Triangle.",
+                "<a href='http://banditosburritolounge.com/' target='_blank'>Bandito's Website</a>"],
+    37.5593 : ["Chiocca's",
+                "Cool little bar and deli in the basement of a house.  One time Seth fell asleep in the alley behind it and Zoe freaked out because she couldn't find him.",
+                "<a href='http://places.singleplatform.com/chioccas/menu?ref=google' target='_blank'>Chiocca's Menu</a>"],
+    37.5560 : ["VMFA",
+                "The Virginia Museum of Fine Arts.  Open every day of the year with free admission to normal exhibits!  On Friday there is happy hour on the lawn.  Get some wine and chill.",
+                "<a href='https://www.vmfa.museum/' target='_blank'>VMFA website</a>"],
+    37.5289 : ["Belle Isle",
+                "Hiking, fishing, swimming, and drinking. This place is awesome to waste the day at.  Bring a towel and find a rock.",
+                ""],
+}
+
+
 const API_KEY = 'pk.eyJ1Ijoic25ld2IiLCJhIjoiY2p2cHNmMG5pMDAwczQ4cnhva3loeXVpbyJ9.KhEIUGLHoHGy2GvWHTfDjw';
-
-// API_KEY is stored in config.js file
-
-// Query URL
-var URL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
-
 
 
 
 var boundary = new L.LayerGroup();
 
-function check() {
-    console.log(this._latlng.lat);
+function showBlurb() {
+
+    document.getElementById("locationName").innerText = blurbInfo[this._latlng.lat][0];
+    document.getElementById("locationBlurb").innerText = blurbInfo[this._latlng.lat][1];
+    document.getElementById("locationWebsite").innerHTML = blurbInfo[this._latlng.lat][2];
 }
 
 
 function dataMap(){
     var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
         id: "mapbox.streets",
         accessToken: API_KEY
     });
-
 
     var myMap = L.map('map', {
         center: [
@@ -31,13 +52,28 @@ function dataMap(){
         zoom: 13,
         layers: [streetmap]
     });
+    
 
-    L.marker([37.5599, -77.4737]).addTo(myMap)
-		.bindPopup("<b>Bandito's Burrito Lounge</b><br />Margaritas and Tacos.").on("click", check);
+    var locations = [
+		["Bandito's", 37.5599, -77.4737],
+		["Chiocca's", 37.5593, -77.4762],
+		["VMFA", 37.5560, -77.474],
+        ["Belle Isle", 37.5289, -77.4526],
+        
+		];
 
+        
+    locations.forEach(el => {
+        marker = new L.marker([el[1], el[2]])
+				.bindPopup(el[0])
+                .addTo(myMap)
+                .on("click", showBlurb);
+    })
         
 
 }
 
 dataMap();
+
+
 
